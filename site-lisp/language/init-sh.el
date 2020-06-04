3d9b4881bb9a059a22df7f52bac5dda878d98cbd
Iorest <vincent.leaforest@gmail.com>
2020-06-04 16:00:03 +0800

    add useful shell command

--- site-lisp/language/init-sh.el
+++ site-lisp/language/init-sh.el
@@ -12,11 +12,14 @@
   :ensure t
   :init
   :bind (("C-x t" . multi-term))
-  :config (setq multi-term-program "/bin/zsh"
+  :config (setq multi-term-program "/bin/bash"
                 multi-term-buffer-name "MTerm"
                 multi-term-scroll-show-maximum-output 0
                 term-unbind-key-list '("C-h" "M-x" "C-x" "C-z")
                 term-term-name "xterm-256color"))
+(use-package emamux
+  :ensure t
+  :config)
 
 (use-package eshell-git-prompt
   :ensure t
@@ -28,6 +31,15 @@
 (use-package vterm
   :ensure t
   :defer t)
+
+(defun open-dir-in-iterm ()
+  "Open the current directory of the buffer in iTerm."
+  (interactive)
+  (let* ((iterm-app-path "/Applications/iTerm.app")
+         (iterm-path (if (file-directory-p iterm-app-path)
+                         iterm-app-path)))
+    (shell-command (concat "open -a " iterm-path " ."))))
+(global-set-key (kbd "C-x C-w") 'open-dir-in-iterm)
 
 (setq system-uses-terminfo nil)
 
